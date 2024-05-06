7d3e0d868310fb4f18f22fb34bf3f27a5fc24752
antra bansal <bansalantra21@gmail.com.com>
2024-05-06 22:12:35 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -16,6 +16,11 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+from components.data_transformation import DataTransformation
+from components.data_transformation import DataTransformationConfig
+
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
@@ -56,7 +61,12 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-    obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+    '''modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))'''
 
 
